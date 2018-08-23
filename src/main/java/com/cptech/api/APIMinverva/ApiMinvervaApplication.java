8c224a660bd755dfd274162cfa4c212b19746042
Aluno <Aluno@Lab07Maq14>
2018-08-23 05:58:25 -0400

    PrimeiroComitt

---
+++ src/main/java/com/cptech/api/APIMinverva/ApiMinvervaApplication.java
@@ -0,0 +1,12 @@
+package com.cptech.api.APIMinverva;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiMinvervaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiMinvervaApplication.class, args);
+	}
+}
