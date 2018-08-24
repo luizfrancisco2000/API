c18055305a16fd873eae54598dd20823a1fc17a9
Luiz Francisco <luizfrancisco2000@gmail.com>
2018-08-24 16:56:38 -0400

    Ultimas modificações

--- app/src/main/java/cptech/com/controltutor/DAO/CodigoDAO.java
+++ app/src/main/java/cptech/com/controltutor/DAO/CodigoDAO.java
@@ -3,62 +3,30 @@ package cptech.com.controltutor.DAO;
 
 import android.os.AsyncTask;
 
-import java.io.IOException;
-import java.net.URI;
-import java.net.URISyntaxException;
+import org.springframework.http.ResponseEntity;
 
 import cptech.com.controltutor.Controle.Codigo;
 import cptech.com.controltutor.Controle.Discente;
-import cz.msebera.android.httpclient.HttpEntity;
-import cz.msebera.android.httpclient.HttpRequest;
-import cz.msebera.android.httpclient.HttpResponse;
-import cz.msebera.android.httpclient.client.methods.HttpPost;
-import cz.msebera.android.httpclient.client.utils.URIBuilder;
-import cz.msebera.android.httpclient.impl.client.DefaultHttpClient;
-import cz.msebera.android.httpclient.params.HttpParams;
-import cz.msebera.android.httpclient.util.EntityUtils;
 
 /**
  * Created by Aluno on 13/08/2018.
  */
 
-public class CodigoDAO {
+public class CodigoDAO extends AsyncTask<Codigo, Integer, ResponseEntity<Codigo>>{
     /**
      * Comandos para fazer a função desejada lá nos servlet
      */
-    private static final String SALVAR = "salvar";
-    private static final String DELETAR = "deletar";
-    private static final String ATUALIZAR = "atualizar";
-    private static final String FIND_BY_ID = "procurarID";
-    private static final String FIND_BY_DISCENTE = "procurarByDiscente";
-    private static final String LISTAR_TODOS = "Listar";
 
     /**
      *URL da página*/
-    private static final String URL = "localhost:8084/APIAndroid/CodigoDAO";
+    private static String URL = "localhost:8000/api/discente";
+
 
     /**
      * Atributos para fazer o programa rodar*/
 
-    /*
-                try {
-                URIBuilder builder = new URIBuilder("http://192.168.43.233/MyProjectAndroid/hello");   //.net servlet url here                builder.setParameter("Name","John Smith");//    this is parameter                DefaultHttpClient httpClient = new DefaultHttpClient();
-                HttpGet httpGet = new HttpGet(builder.build());
-                HttpResponse httpResponse = httpClient.execute(httpGet);
-                HttpEntity httpEntity = httpResponse.getEntity();
-                output = EntityUtils.toString(httpEntity);
-            } catch (UnsupportedEncodingException e) {
-                e.printStackTrace();
-            } catch (ClientProtocolException e) {
-                e.printStackTrace();
-            } catch (IOException e) {
-                e.printStackTrace();
-            } catch (URISyntaxException e) {
-                e.printStackTrace();
-            }
-            return output;
-        }*/
-    public String adicionar(Codigo codigo) throws URISyntaxException, IOException {
+    public String adicionar(Codigo codigo){
+
         return null;
     }
 
@@ -71,6 +39,10 @@ public class CodigoDAO {
     }
 
     public Discente buscarByID() {
+        return null;
+    }
+    @Override
+    protected ResponseEntity<Codigo> doInBackground(Codigo... codigos) {
         return null;
     }
 }
