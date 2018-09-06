5f5d9061009ec8f49de15e4816bd25f8a01d3200
Maria Elisa <limselisa@gmail.com>
2018-09-06 16:59:08 -0400

    Sei fazer mais que mudar id

---
+++ app/src/main/java/cptech/com/controltutor/PerfilAlunoActivity.java
@@ -0,0 +1,13 @@
+package cptech.com.controltutor;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PerfilAlunoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_perfil_aluno);
+    }
+}
