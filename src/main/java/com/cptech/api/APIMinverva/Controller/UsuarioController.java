174214b8f6e66fccf98f70341a343d7a62540fbf
Luiz Francisco <luizfrancisco2000@gmail.com>
2018-10-04 15:30:45 -0400

    Api com cadastro de alunos já

--- src/main/java/com/cptech/api/APIMinverva/Controller/UsuarioController.java
+++ src/main/java/com/cptech/api/APIMinverva/Controller/UsuarioController.java
@@ -5,10 +5,31 @@
  */
 package com.cptech.api.APIMinverva.Controller;
 
+import com.cptech.api.APIMinverva.Repository.DiscenteRepository;
+import com.cptech.api.APIMinverva.Repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  *
  * @author Aluno
  */
+
+@RestController
+@RequestMapping("/api")
 public class UsuarioController {
+    
+    @Autowired
+    UsuarioRepository userRepository;
+        
+    @RequestMapping(method = RequestMethod.GET, path = "/usuario/procurar/{id}")
+    public ResponseEntity<?> getAllUsuario(@PathVariable("id") long id){
+        return new ResponseEntity<>(userRepository.findById(id),HttpStatus.OK);
+    }
     
 }
