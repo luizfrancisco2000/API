da9aed785489f99dfd4b48765ac14439ad62bb76
Maria Elisa <limselisa@gmail.com>
2018-10-09 17:18:05 -0400

    Telas e listas

---
+++ app/src/main/java/cptech/com/controltutor/Interface/Discente/ListaProfessor.java
@@ -0,0 +1,15 @@
+package cptech.com.controltutor.Interface.Discente;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import cptech.com.controltutor.R;
+
+public class ListaProfessor extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_professor);
+    }
+}
---
+++ app/src/main/java/cptech/com/controltutor/Interface/Discente/MateriasTutor.java
@@ -0,0 +1,13 @@
+package cptech.com.controltutor.Interface.Discente;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MateriasTutor extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_materias_tutor);
+    }
+}
