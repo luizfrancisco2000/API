bb2d82054ba1750c2b5fd65c171d3cbeddb34201
luizfrancisco2000 <36229890+luizfrancisco2000@users.noreply.github.com>
2018-10-15 07:55:54 -0400

    Update DiscenteRestClient.java

--- app/src/main/java/cptech/com/controltutor/Connect/DiscenteRestClient.java
+++ app/src/main/java/cptech/com/controltutor/Connect/DiscenteRestClient.java
@@ -18,8 +18,9 @@ import cptech.com.controltutor.Controle.Discente;
 
 public class DiscenteRestClient {
     //http://10.100.37.192:8000
+    //http://10.100.38.128:8000
     //http://10.100.45.241:8000
-    private final String BASE_URL = "http://10.100.39.16:8000/api/discente/";
+    private final String BASE_URL = "http://10.100.38.128:8000/api/discente/";
     private RestTemplate restTemplate;
     private String url;
 
