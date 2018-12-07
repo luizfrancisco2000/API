0e5a80cec460471c0385ac94a4ed503cca742092
luizfrancisco2000 <36229890+luizfrancisco2000@users.noreply.github.com>
2018-12-07 14:38:59 -0400

    Rename

--- Tentativa.java
+++ Tentativa.java
@@ -1,5 +1,7 @@
- holder.btnDelete.setOnClickListener(view -> apagarConvite(holder.getAdapterPosition(), convite));
+public void onBindViewHolder{
+holder.btnDelete.setOnClickListener(view -> apagarConvite(holder.getAdapterPosition(), convite));
 
             holder.btnRecusar.setOnClickListener(view -> apagarConvite(holder.getAdapterPosition(), convite));
 
             holder.btnAceitar.setOnClickListener(view -> aceitarConvite(holder.getAdapterPosition(), convite));
+}
